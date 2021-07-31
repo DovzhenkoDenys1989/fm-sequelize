6563f6a92634142b1d07e0d7ec196d6f3da1a29f
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-07-31 16:18:01 +0300

    Add pagination mw

--- middlewares/error.handlers.mw.js
+++ middlewares/error.handlers.mw.js
@@ -1,5 +1,6 @@
 module.exports = async (err, req, res, next) => {
   const status = err.status || 500;
+  console.error(err);
 
   res.status(status).send({
     errors: [{ message: err || 'Server error' }],
